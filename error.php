09cde967888efdb5e32809933e3278a5e01c5eb6
David Frassi <info@davidfrassi.it>
2017-10-10 14:47:31 +0200

    aggiunta descrizione di funzionamento

--- error.php
+++ error.php
@@ -1,4 +1,12 @@
 <!DOCTYPE html>
+<!--
+
+Questa pagina error.php redirige ogni errore 404 alla pagina del cms /pagina-non-trovata
+è necessario quindi creare un articolo su Joomla che abbia alias "pagina-non-trovata"
+all'interno del quale scrivere il messaggio specifico di errore da mostrare all'utente
+
+
+-->
 <html lang="it-it">
 <head>
 <script>window.location.replace("pagina-non-trovata");</script>
